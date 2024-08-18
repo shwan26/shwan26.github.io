241241ac5d622ae18a8c4bbe06ff69d6ef6f79fc
Shwan <syanshwan.26@gmail.com>
2024-08-18 22:41:51 +0700

    updated

--- react-routing/src/routes/root.jsx
+++ react-routing/src/routes/root.jsx
@@ -13,7 +13,7 @@ import { useEffect } from "react";
 
 export async function action() {
   const contact = await createContact();
-  return redirect('/routing-app/contacts/${contact.id}/edit');
+  return redirect(`/routing-app/contacts/${contact.id}/edit`);
 }
 
 export async function loader({request}) {
